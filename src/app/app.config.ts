2d314daa30b662166a327f3d8090251b8f2a3f7e
Marco <marco.schoch@gmx.ch>
2026-09-11 18:50:12 +0700

    #IMPROVEMENT add error intercepter to log failed api calls

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './errorhandling/error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor]))
   ]
 };
